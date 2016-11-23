70bb7ffaaeae332432b5fb20abdde9340ced3a54
hadoop <python35@163.com>
2016-11-23 17:28:59 +0800

    ipython 的 spark 单词统计

--- Spark/wordcount.py
+++ Spark/wordcount.py
@@ -7,12 +7,15 @@ from pyspark import SparkContext
 from operator import add
 import sys
 
+import re
+
 if __name__ == "__main__":
 
     #conf = SparkConf().setMaster("local").setAppName("My App")
     sc = SparkContext(appName="PythonWordCount")
-    lines = sc.textFile('data/words.txt')
-    counts = lines.flatMap(lambda x: x.split(' ')) \
+    lines = sc.textFile('data/shakespeare.txt')
+    #re.split(r'(?:,|;|\s)\s*', line)  #x.split(' ')) \
+    counts = lines.flatMap(lambda x: re.split(r'(?:\W)\W*',x)) \
                   .map(lambda x: (x,1)) \
                   . reduceByKey(add)
     output = counts.collect()
