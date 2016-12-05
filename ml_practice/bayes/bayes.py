d5872fc96d336f3934e26c96d373419ea5f07155
thinkpad <python35@163.com>
2016-12-06 01:26:34 +0800

    机器学习实战--贝叶斯网络

---
+++ ml_practice/bayes/bayes.py
@@ -0,0 +1,35 @@
+#-*- coding:utf-8 -*-
+
+def loadDataSet():
+    postingList = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
+                   ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+                   ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+                   ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+                   ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
+                   ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
+    classVec = [0,1,0,1,0,1]  # 1 代表侮辱性文字，0代表正餐言论
+    return postingList,classVec
+
+
+# 创建一个不含重复单词的 set
+def createVocabList(dataSet):
+    vocabSet = set([])
+    for document in dataSet:
+        vocabSet = vocabSet | set(document)
+    return list(vocabSet)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
