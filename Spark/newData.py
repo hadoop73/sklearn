c3ef30ba66d7fbd74598e8baacccf509c62930e1
hadoop <python35@163.com>
2016-12-13 14:58:56 +0800

    slope One

---
+++ Spark/newData.py
@@ -0,0 +1,42 @@
+# coding:utf-8
+
+
+
+
+import numpy as np
+
+
+
+import pandas as pd
+import numpy as np
+
+
+train = pd.read_csv('data/train.csv',nrows=1000,header=None)
+
+train.to_csv('data/train_slope1000.csv',header=None,index=None)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
