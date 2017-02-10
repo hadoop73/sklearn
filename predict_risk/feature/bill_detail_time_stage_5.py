ecc3e76a9e47a73b2737c8b452d3f522639fd7b4
hadoop <python35@163.com>
2017-02-11 00:12:48 +0800

    bill_stage_5

--- predict_risk/feature/bill_detail_time_stage_5.py
+++ predict_risk/feature/bill_detail_time_stage_5.py
@@ -11,12 +11,26 @@ names = ["userid", "time", "bank_id", "pre_amount_of_bill", "pre_repayment", "cr
 
 bill_train = pd.read_csv("../../pcredit/train/bill_detail_train.txt", header=None)
 bill_test = pd.read_csv("../../pcredit/test/bill_detail_test.txt", header=None)
-
 bill_data = pd.concat([bill_train, bill_test])
+
+del bill_train, bill_test
 bill_data.columns = names
 
-bill_data.loc[bill_data['consume_amount']>60,'consume_amount'] = 60
-bill_data.loc[bill_data['prepare_amount']>25,'prepare_amount'] = 23
+
+bill_data = bill_data[bill_data['time']>5840000000]
+bill_data.loc[bill_data['consume_amount']>12,'consume_amount'] = 12
+#bill_data.loc[bill_data['prepare_amount']>25,'prepare_amount'] = 23
+
+names_loan_time = ['userid','loan_time']
+loan_time_train = pd.read_csv("../../pcredit/train/loan_time_train.txt",header=None)
+loan_time_test = pd.read_csv("../../pcredit/test/loan_time_test.txt",header=None)
+
+loan_time = pd.concat([loan_time_train,loan_time_test],axis=0)
+del loan_time_train,loan_time_test
+loan_time.columns = names_loan_time
+
+bill_data = pd.merge(bill_data,loan_time,on='userid')
+del loan_time
 
 stage = ['stg1_','stg2_','stg3_','stg4_','stg5_']
 
@@ -24,27 +38,21 @@ cols = ['time','pre_amount_of_bill', 'pre_repayment', 'credit_amount',
         'amount_of_bill_left', 'least_repayment', 'consume_amount',
         'amount_of_bill', 'adjust_amount', 'circ_interest', 'avail_amount',
         'prepare_amount']
-sts = ['_min', '_max', '_median', '_mean', '_std', '_cnt', '_max_min']
-
-features = pd.DataFrame(columns=[s+col+st for st in sts for s in stage for col in cols])
 
 from multiprocessing import Pool,Queue,Lock
 
+#  获得 10% 20% 到 90% 的数据分割点
+t = bill_data['time'].describe(percentiles=[0.2, 0.4, 0.6, 0.8])
+split_point = [0, int(t['20%']), int(t['40%']), int(t['60%']), int(t['80%']), 1e11]
 
 def test_(u):
 
-    #  获得 10% 20% 到 90% 的数据分割点
-    t = bill_data['time'].describe(percentiles=[0.2,0.4,0.6,0.8])
-
-    split_point = [0,int(t['20%']),int(t['40%']),int(t['60%']),int(t['80%']),1e11]
-
     # 获得所有的用户
     d = {'userid': u}
-
     for col in cols:
         bill_user = bill_data[bill_data.userid==u]
-        data = bill_user[bill_user[col] != 0]
-
+        if col not in ['consume_amount']:
+            data = bill_user[bill_user[col] != 0]
         for i in range(5):
                 stg = stage[i]
                 di = data[(split_point[i]<(data.time))&((data.time)<split_point[i+1])]
@@ -59,53 +67,120 @@ def test_(u):
                 d[stg+col+'_cnt_t5'] = di[col].count()
                 d[stg+col+'_var_t5'] = di[col].var()
                 d[stg+col+'_max_min_t5'] = di[col].max() - di[col].min()
-    #ftures = pd.DataFrame(d,index=[0])
-    print "add userid: ", u
     print d
     return d
 
+def multi():
+    pool = Pool(12)
+    #  按照特征咧进行处理
+    users = list(bill_data.userid.unique())
+    rst = pool.map(test_,users)
+    pool.join()
+    pool.close()
+    features = pd.DataFrame(rst)
+
+    #    features = pd.merge(features,i,on='userid',how='outer')
+    # 效果不错 0.44
+    #features.to_csv('../data/train/bill_detail_time_stage5.csv',index=None)
 
 
-rst = []
-pool = Pool(12)
-#  按照特征咧进行处理
+    print features.head()
+    print features.shape
 
-users = list(bill_data.userid.unique())
+    features.to_csv('../data/train/bill_dt_time_5.csv',index=None)
 
-for u in users:
-    #print "add userid: ",u
-    rst.append(pool.apply_async(test_,args=(u,)))
-pool.close()
-pool.join()
+    #features.to_csv('../data/train/bill_detail_time_stage52.csv',index=None)
 
-rst = [i.get() for i in rst]
+# 统计 5 个时间段的信用卡数量
+def stage_5_bank():
+    data = bill_data[['userid','time','bank_id']]
+    dd = None
+    for i in range(5):
+        stg = stage[i]
+        di = data[(split_point[i] < (data.time)) & ((data.time) < split_point[i + 1])]
+        d = di[['userid','bank_id']]
+        d.drop_duplicates(inplace=True)
+        d['bank_id'] = d['bank_id'].astype('str')
+        #f = d.groupby('userid')['bank_id'].agg(lambda x:":".join(x)).reset_index()
+        #f.rename(columns={'bank_id':stg+'bankids'},inplace=True)
+        f = d.groupby('userid')['bank_id'].agg(lambda x: len(x)).reset_index()
+        f.rename(columns={'bank_id': stg + 'bank_cnt'},inplace=True)
+        if i==0:
+            dd = f
+        else:
+            dd = pd.merge(dd,f,how='outer')
 
-#features = pd.DataFrame(columns=[ st+s+p for p in sts  for st in stage for s in cols])
+    print dd.head()
+    print dd.shape
+    dd.to_csv("../data/train/bill_stage_bank.csv",index=None)
 
-features = pd.DataFrame(rst)
+# 统计放款前后的信用卡数量,以及放款后信用卡新增数量
+def time_split_bank():
+    data = bill_data[bill_data.time<bill_data.loan_time]
+    data = data[['userid','bank_id']]
+    data.drop_duplicates(inplace=True)
+    f = data.groupby('userid')['bank_id'].agg(lambda x:len(x)).reset_index()
+    f.rename(columns={'bank_id':'before_bank_cnt'},inplace=True)
 
-#for i in range(l):
-    #features.loc[i] =rst[i]
-#    features.append(rst[i],ignore_index=True)
+    data = bill_data
+    data = data[['userid', 'bank_id']]
+    data.drop_duplicates(inplace=True)
+    ff = data.groupby('userid')['bank_id'].agg(lambda x: len(x)).reset_index()
+    ff.rename(columns={'bank_id': 'all_bank_cnt'}, inplace=True)
 
-#features = reduce(lambda x,y:pd.concat([x,y],axis=0),rst)
+    f = pd.merge(f,ff,on='userid',how='outer')
+    f['bank_add'] = f['all_bank_cnt'] - f['before_bank_cnt']
+    print f.head()
+    print f.shape
+    f.to_csv("../data/train/bill_all_bank_add.csv",index=None)
 
-#for i in rst[1:]:
-#    features = pd.merge(features,i,on='userid',how='outer')
-# 效果不错 0.44
-#features.to_csv('../data/train/bill_detail_time_stage5.csv',index=None)
+# 统计每个阶段的信用卡数量,以及每个阶段新增数量
+def stage_m5_bank():
+    data = bill_data[['userid','time','bank_id']]
+    dd = None
+    for i in range(5):
+        stg = stage[i]
+        di = data[(data.time) < split_point[i + 1]]
+        d = di[['userid','bank_id']]
+        d.drop_duplicates(inplace=True)
+        d['bank_id'] = d['bank_id'].astype('str')
+        #f = d.groupby('userid')['bank_id'].agg(lambda x:":".join(x)).reset_index()
+        #f.rename(columns={'bank_id':stg+'bankids'},inplace=True)
+        f = d.groupby('userid')['bank_id'].agg(lambda x: len(x)).reset_index()
+        f.rename(columns={'bank_id': stg + 'mbank_cnt'},inplace=True)
+        if i==0:
+            dd = f
+        else:
+            dd = pd.merge(dd,f,how='outer')
+    dd['all_mbank_cnt'] = dd['stg5_mbank_cnt'] - dd['stg1_mbank_cnt']
+    dd['stg5_mbank_cnt'] = dd['stg5_mbank_cnt'] - dd['stg4_mbank_cnt']
+    dd['stg4_mbank_cnt'] = dd['stg4_mbank_cnt'] - dd['stg3_mbank_cnt']
+    dd['stg3_mbank_cnt'] = dd['stg3_mbank_cnt'] - dd['stg2_mbank_cnt']
+    dd['stg2_mbank_cnt'] = dd['stg2_mbank_cnt'] - dd['stg1_mbank_cnt']
+    dd.drop('stg1_mbank_cnt',axis=1,inplace=True)
+    print dd.head(100)
+    print dd.shape
+    dd.to_csv("../data/train/bill_stagem_bank.csv",index=None)
 
 
-print features.head()
-print features.shape
+def merge_aa():
+    a = pd.read_csv("../data/train/bill_stage_bank.csv")
+    b = pd.read_csv("../data/train/bill_all_bank_add.csv")
+    c = pd.read_csv("../data/train/bill_stagem_bank.csv")
 
+    d = pd.merge(a,b,on='userid',how='outer')
+    d = pd.merge(d,c,on='userid',how='outer')
+    print d.head()
+    print d.shape
+    d.to_csv("../data/train/bill_bank_aaad.csv",index=None)
 
-features.to_csv('../data/train/bill_detail_time_stage52.csv',index=None)
-
-
-
-
-
+if __name__=='__main__':
+    import warnings
+    warnings.simplefilter('ignore')
+    #stage_5_bank()
+    #time_split_bank()
+    #stage_m5_bank()
+    merge_aa()
 
 
 
