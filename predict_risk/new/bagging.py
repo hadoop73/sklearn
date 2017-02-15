90934703b3060cb43cf1c8031371044e6a7e3d04
hadoop <python35@163.com>
2017-02-15 21:42:26 +0800

    bagging

--- predict_risk/new/bagging.py
+++ predict_risk/new/bagging.py
@@ -24,16 +24,16 @@ target.columns = ['userid', 'label']
 def bagging3():
     preds = pd.read_csv('data/train_test_pred.csv')
     cols = list(preds.columns)
-    kks = 0
-    auc = 0
     nn = np.arange(0,105,5)
     for i in range(1,len(cols)-2):
         for j in range(i+1,len(cols)-1):
             for k in range(j+1, len(cols)):
+                kks = 0
+                auc = 0
                 for m in range(20):
                     wa = random.choice(nn)
                     wb = random.choice(nn)
-                    if wa + wb > 100:
+                    while wa + wb > 100:
                         if wa > wb:
                             wa = wa - 50
                         else:
@@ -43,21 +43,17 @@ def bagging3():
                     wa = 0.01 * wa
                     wb = 0.01 * wb
                     wc = 0.01 * wc
-                    print wa,wb,wc
-                    print preds[cols[i]].head()
-                    print preds[cols[j]].head()
-                    print preds[cols[k]].head()
                     preds['probability'] = wa*preds[cols[i]] + wb*preds[cols[j]] + wc*preds[cols[k]]
-
                     t = preds[['userid','probability']]
                     train = t.iloc[0: 55596]
-                    print train.head()
                     test = t.iloc[55596:]
-                    print test.head()
 
                     ks = KS(train['probability'],target.label)
                     fp, tp, thresholds = metrics.roc_curve(target.label, train['probability'], pos_label=1)
                     au = metrics.auc(fp, tp)
+                    print wa, wb, wc
+                    print "KS: ",ks
+                    print "AUC: ",au
                     if ks >= kks or au >= auc:
                         kks = max(kks,ks)
                         auc = max(au,auc)
