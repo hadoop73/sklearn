4ca6f57c9998c96f8aef8e96be5851992fe4758d
jqk2017 <2034263349@qq.com>
2017-03-16 21:18:12 +0800

    pic

---
+++ PIC/lda_pic.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+from pandas import DataFrame,Series
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+df = pd.DataFrame({
+    'K':[],
+    'Perplexity-Var':[]
+})
+
+
+
+
+
+
